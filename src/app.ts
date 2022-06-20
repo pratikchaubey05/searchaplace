629702a3a35f95d72bde0d8ac7da8d33a8c7e84c
pratikchaubey <pratik.chaubey@encora.com>
2022-06-21 01:33:28 +0530

    1.1: Added Googlegeocoding api to search for location and google js map api to display the map.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,43 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+//Note: Use geocoding to generate a API key
+const GOOGLE_API_KEY = "AIzaSyCIaAc2c5M3VpbCH6PPq_guwy91HuowXOs";
+declare var google:any;
+
+type GoogleGeoCodingResponse = {
+  results: { geometry: { location: {lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+}
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value ;
 
-  // Need to work further
+  axios.get<GoogleGeoCodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+  .then((response) => {
+    console.log(response);
+    if(response.data.status !== "OK"){
+      throw new Error("Could not fetch location!");
+    }
+    const coordinates = response.data.results[0].geometry.location;
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: coordinates,
+      zoom: 8
+    });
+
+    new google.maps.Marker({
+      position: coordinates,
+      map: map,
+    });
+    
+  })
+  .catch(err => {
+    alert(err.message);
+    console.log(err)
+  });
 }
 
 
